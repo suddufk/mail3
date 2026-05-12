abd4767b9aad0c0ebd149f26dc9235db8d77c565
LxhNolan <hprnolan1649@foxmail.com>
2026-05-12 22:24:44 +0800

    Update Japanese language import in i18n setup
    Replaced 'jp' with 'ja' for Japanese language import.

--- mail-vue/src/i18n/index.js
+++ mail-vue/src/i18n/index.js
@@ -1,7 +1,7 @@
 import { createI18n } from 'vue-i18n';
 import en from './en.js'
 import zh from './zh.js'
-import jp from './jp.js'
+import ja from './ja.js'
 const i18n = createI18n({
     legacy: false,
     messages: {
